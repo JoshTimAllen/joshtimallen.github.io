c5f5a4e0e9bfdd86395bc76f9b09ba80421d959c
TheeJT <40251814+TheeJT@users.noreply.github.com>
2018-11-25 18:07:24 -0500

    n

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -4,18 +4,24 @@ var showingMenu = false;
 
 function ShowMenu(){
   var m = document.getElementById("Menu");
+  var h = document.getElementById("HH");
   if(showingMenu == false){
     showingMenu = true;
       m.style.display="inline";
+      h.style.height="100%";
+      h.style.background= "rgba(0, 0, 0,0.7)";
   }
   else {
     showingMenu = false;
     m.style.display="none";
+    h.style.height="45px";
+    h.style.background= "rgba(0, 0, 0,0.5)";
   }
 }
 
 function mediaQuery(x){
   var m = document.getElementById("Menu");
+  var h = document.getElementById("HH");
   if (x.matches) { // If media query matches
     showingMenu = false;
     m.style.display="none";
@@ -23,6 +29,8 @@ function mediaQuery(x){
     else {
     showingMenu = true;
     m.style.display="inline";
+    h.style.height="45px";
+    h.style.background= "rgba(0, 0, 0,0.5)";
     }
 }
 
