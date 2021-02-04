552e2eca8377e1135c2a514dd031065c8611b7f2
JoshTimAllen <40319513+JoshTimAllen@users.noreply.github.com>
2021-02-04 12:19:18 -0500

    Add files via upload

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,44 +1,8 @@
-
-var showingMenu = false;
-
-
-function ShowMenu(){
-  var m = document.getElementById("Menu");
-  var h = document.getElementById("HH");
-  var b = document.getElementById("body");
-  if(showingMenu == false){
-    showingMenu = true;
-      m.style.display="inline";
-      h.style.height="100%";
-      h.style.background= "rgba(0, 0, 0,0.7)";
-      b.style.overflow = "hidden";
-  }
-  else {
-    showingMenu = false;
-    m.style.display="none";
-    h.style.height="45px";
-    h.style.background= "rgba(0, 0, 0,0.5)";
-    b.style.overflow = "auto";
-  }
+function parrallax(element, distance, speed){
+    const item = document.querySelector(element);
+    item.style.transform = `translateY(${distance * speed}px)`;
 }
 
-function mediaQuery(x){
-  var m = document.getElementById("Menu");
-  var h = document.getElementById("HH");
-  var b = document.getElementById("body");
-  if (x.matches) { // If media query matches
-    showingMenu = false;
-    m.style.display="none";
-    }
-    else {
-    showingMenu = true;
-    m.style.display="inline";
-    h.style.height="45px";
-    h.style.background= "rgba(0, 0, 0,0.5)";
-    b.style.overflow = "auto";
-    }
-}
-
-var x = window.matchMedia("(max-width: 900px)");
-mediaQuery(x);
-x.addListener(mediaQuery);
+window.addEventListener('scroll',function(){
+    parrallax("section",window.scrollY,1);
+});
