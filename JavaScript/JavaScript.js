deb2e27089ba519711bf6d1e131b99b9be71bd16
Josh-Tim Allen <josh-tim_allen@hotmail.com>
2022-08-31 21:40:15 -0500

    Complete redesign
    Complete redesign

--- JavaScript/JavaScript.js
+++
@@ -1,8 +0,0 @@
-function parrallax(element, distance, speed){
-    const item = document.querySelector(element);
-    item.style.transform = `translateY(${distance * speed}px)`;
-}
-
-window.addEventListener('scroll',function(){
-    parrallax("section",window.scrollY,1);
-});
